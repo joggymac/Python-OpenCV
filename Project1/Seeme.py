d5fa8de3ff10ca89fa7add3bd6f01eee629853ba
designer <designer@iMac-2015.home>
2018-09-01 12:38:27 -0400

    followme

--- Project1/Seeme.py
+++ Project1/Seeme.py
@@ -4,8 +4,10 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+
 img = cv2.imread('image.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
@@ -15,6 +17,12 @@ for (x,y,w,h) in faces:
     eyes = eye_cascade.detectMultiScale(roi_gray)
     for (ex,ey,ew,eh) in eyes:
         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+# sidefaces = faceside_cascade.detectMultiScale(gray)
+
+# for (sx, sy, sw, sh) in sidefaces:
+ #   cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (0, 255, 0), 2)
+
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
